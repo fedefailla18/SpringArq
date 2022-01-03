6d30af41dcbc91d0c8c1aac6ce57cb6c21875b33
Fede Failla <fedefailla18@gmail.com>
2022-01-03 11:34:36 -0300

    commiting partial changes

--- src/main/java/com/helper/work/utils/GoogleCalendarService.java
+++ src/main/java/com/helper/work/utils/GoogleCalendarService.java
@@ -5,6 +5,7 @@ import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInsta
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
@@ -33,7 +34,6 @@ public class GoogleCalendarService {
     private static final String APPLICATION_NAME = "Google Calendar API Java Quickstart";
     /** Global instance of the JSON factory. */
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    /** Directory to store authorization tokens for this application. */
 
     /**
      * Global instance of the scopes required by this quickstart.
@@ -41,19 +41,21 @@ public class GoogleCalendarService {
      */
     private static final List<String> SCOPES = Collections.singletonList(CalendarScopes.CALENDAR);
 
-
-    private GoogleCalendarService() {
-    }
+    private static final String USER_ID_KEY = "MY_DUMMY_USER";
 
     private NetHttpTransport httpTransport;
     private GoogleAuthorizationCodeFlow flow;
     private Credential user;
 
+    /** Directory to store authorization tokens for this application. */
     @Value("${google.tokens.directory-path}")
     private static String tokensDirectoryPath = "tokens";
 
+    @Value("${google.auth.callback-uri}")
+    private static final String CALLBACK_URI = "http://localhost:8080/";
+
     @Value("${google.credentials.file-path}")
-    private static String credentialsFilePath = "/client_secret.json";
+    private static String credentialsFilePath = "client_secret.json";
 
     private void initializeFlow() throws IOException, GeneralSecurityException {
         // Load client secrets.
@@ -90,5 +92,16 @@ public class GoogleCalendarService {
         return new Calendar.Builder(httpTransport, JSON_FACTORY, credentials)
                 .setApplicationName(APPLICATION_NAME)
                 .build();
+    }
+
+    public void saveAuthCode(String code) throws IOException {
+        GoogleTokenResponse response = flow.newTokenRequest(code)
+                .setRedirectUri(CALLBACK_URI)
+                .execute();
+        flow.createAndStoreCredential(response, USER_ID_KEY);
+    }
+
+    public void getUserCredentials() throws IOException {
+        final Credential credential = flow.loadCredential(USER_ID_KEY);
     }
 }
